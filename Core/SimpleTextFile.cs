252ab1a66d30b25eaa10a5ee8600a88a58997459
KAROLL STEFANNY MOORE LOPEZ <karollmoore1141863@correo.itm.edu.co>
2026-06-05 21:20:20 -0500

    Simple text file

---
+++ Core/SimpleTextFile.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.IO;
+
+namespace Care;
+
+public class SimpleTextFile
+{
+    private readonly string _Path;
+
+    public SimpleTextFile(string path)
+    {
+        _Path = path;
+    }
+
+    public void WriteLines(string[] lines)
+    {
+        var directory = Path.GetDirectoryName(_Path);
+        if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+        {
+            Directory.CreateDirectory(directory);
+        }
+        File.WriteAllLines(_Path, lines);
+    }
+
+
+    public string[] ReadLines()
+    {
+        if (!File.Exists(_Path))
+        {
+            var directory = Path.GetDirectoryName(_Path);
+            if (!string.IsNullOrEmpty(directory) && !Directory.Exists(directory))
+            {
+                Directory.CreateDirectory(directory);
+            }
+            File.WriteAllText(_Path, string.Empty);
+            return Array.Empty<string>();
+        }
+
+        return File.ReadAllLines(_Path);
+    }
+
+}
